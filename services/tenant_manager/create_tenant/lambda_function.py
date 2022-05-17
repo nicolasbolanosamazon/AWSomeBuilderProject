67de313b57df6553334a70f9827440eab90552d0
nicolasbolanosamazon <103776194+nicolasbolanosamazon@users.noreply.github.com>
2022-05-17 11:39:54 -0500

    remove pyhton requirments

--- services/tenant_manager/create_tenant/lambda_function.py
+++ services/tenant_manager/create_tenant/lambda_function.py
@@ -8,6 +8,18 @@ dynamodb = boto3.resource('dynamodb')
 table_tenant_details = dynamodb.Table(environ.get('TABLE_NAME'))
 
 def create_tenant(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers" : "Content-Type, Origin, X-Requested-With, Accept, Authorization, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Access-Control-Allow-Origin",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT"
+        },
+        "body": json.dumps({
+            "message": event
+        }),
+    }
+    """
     tenant_details = json.loads(event['body'])
 
     try:          
@@ -23,3 +35,4 @@ def create_tenant(event, context):
         raise Exception('Error creating a new tenant', e)
     else:
         return utils.create_success_response("Tenant Created")
+    """
